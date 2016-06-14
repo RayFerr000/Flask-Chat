92bbae89d120032e88e61baaceb974a5ecbea755
Raymond Ferranti <rayferranti@gmail.com>
2016-06-14 00:12:28 -0400

    finished setting up gulp for converting jsx into js

--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 # from the app package, import the app variable class. This will grab the app variable from the __init__.py file
+from flask import render_template
 from app import app
 
-#@app.route() is a method of the flask class. Our app variable is an instance of the flask class, thus it has 
-# a route method. A route maps a url to a function.
 @app.route('/')
-def home():
-    return "This is the main url."
+def index():
+    return render_template('main.html')
