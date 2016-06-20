99ece92cec5d79009608a2298a33e7ce2cd77d75
Ray Ferranti <rayferranti@gmail.com>
2016-06-19 23:40:35 -0400

    added login and sign up buttons to header

--- app/static/scripts/jsx/header.jsx
+++ app/static/scripts/jsx/header.jsx
@@ -1,6 +1,11 @@
 class Header extends React.Component {
   render() {
-    return <div className="header"></div>;
+    return (
+      <div className="header">
+        <button className="btn btn-default right-margin">Sign Up</button>
+        <button className="btn btn-default">Login</button>
+      </div>
+    );
   }
 }
 export default Header;
